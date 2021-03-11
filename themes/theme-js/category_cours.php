0af52ebb1933f4cc00502ec35ad430c9766af3d6
Jisei292 <73396433+Jisei292@users.noreply.github.com>
2021-03-11 18:40:21 -0500

    labo 4

---
+++ themes/theme-js/category_cours.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * The template for displaying archive pages
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package theme.JS
+ */
+
+get_header();
+?>
+
+	<main id="primary" class="site-main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
+				<?php
+				the_archive_title( '<h1 class="page-title">', '</h1>' );
+				the_archive_description( '<div class="archive-description">', '</div>' );
+				?>
+			</header><!-- .page-header -->
+
+			<?php
+			$precedent = 0;
+			/* Start the Loop */
+			while ( have_posts() ) :
+				the_post();
+				$titre = get_the_title();
+				$session = substr($titre, 4,1);
+				$contenu = substr(get_the_content();)
+				$resume = substr($contenu, 0, 200);
+				?>
+
+				<?php
+				if ($session != $precedent){
+					echo "<p>Session : " . $session . "</p>";
+				}
+				$precedent = $session;
+
+				?>
+
+				<p> <?php echo $session . " - " . $titre; ?> </p>
+				<p> <?php echo $resume; ?>  </p>
+				<?php
+
+			endwhile;
+
+		
+
+		endif;
+		?>
+
+	</main><!-- #main -->
+
+<?php
+get_sidebar();
+get_footer();
